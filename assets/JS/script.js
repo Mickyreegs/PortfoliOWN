1136ab1f535d244e9e15f6abd57705bb3ba0dff6
Michael O'Regan <w58fyus5@students.codeinstitute.net>
2024-12-01 13:40:01 +0000

    Add dropdown and buttons to build your portfolio section

---
+++ assets/JS/script.js
@@ -0,0 +1,24 @@
+
+// Get the button elements and add event listeners to them
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for(let button of buttons){
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                checkAnswer();
+            } else{
+                let gameType = this.getAttribute("data-type");
+                runGame(gameType);
+            }
+        })
+    }
+
+    document.getElementById("answer-box").addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            checkAnswer();
+        }
+    })
+
+    runGame("addition");
+})
